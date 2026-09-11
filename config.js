0a1c43d237455b14d0652a2c7e981800dccc9a3a
ujja5772 <ujja5772@gmail.com>
2026-09-11 10:48:14 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase 프로젝트 설정
 // Supabase 대시보드 > Project Settings > API 에서 값을 복사해서 아래에 붙여넣어 주세요.
-const SUPABASE_URL = "여기에_Project_URL_붙여넣기"; // 예: https://xxxxxxxxxxxx.supabase.co
-const SUPABASE_ANON_KEY = "여기에_anon_public_키_붙여넣기";
+const SUPABASE_URL = "https://msdvdvdkiowbdugvzpad.supabase.co/rest/v1/"; // 예: https://xxxxxxxxxxxx.supabase.co
+const SUPABASE_ANON_KEY = "sb_publishable_FYnFfUnIs5rcJ0Xv-7tcNg_eHPTb6xs";
